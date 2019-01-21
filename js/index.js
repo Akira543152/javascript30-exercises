469d5def28108f5e23b22c442d2d419fe8775b37
ming <lion543152@gmail.com>
2019-01-21 11:11:13 +0800

    improve-2

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function playsound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return; //stop the function from running all togethen
+  //console.log(e.keyCode);
   audio.currentTime = 0; //rewind to the start 可連續打擊，不用等音效播完
   audio.play();
   key.classList.add("playing");
